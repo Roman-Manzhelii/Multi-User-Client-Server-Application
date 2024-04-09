c5de179a2bdacfe91c7891e0ada2bcc2f29c124c
Elga J Henry <henryelga2004@gmail.com>
2024-04-09 12:23:19 +0100

    Added Feature 3, to delete player by ID Updated main app to include option to delete

--- src/main/java/com/dkit/oop/sd2/DAOs/PlayerDaoInterface.java
+++ src/main/java/com/dkit/oop/sd2/DAOs/PlayerDaoInterface.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface PlayerDaoInterface
 {
     List<Player> getAllPlayers() throws DaoException;
-    public Player deletePlayer(int id) throws DaoException;
+    Player deletePlayer(int id) throws DaoException;
     Player insertPlayer(Player p) throws DaoException;
     Player updatePlayer(int id, Player p) throws DaoException;
     boolean exists(int id) throws DaoException;
