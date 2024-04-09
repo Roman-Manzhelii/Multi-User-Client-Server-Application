3a019c5937ad5931989457ab1fdb475266de805d
milamocha <d00253640@student.dkit.ie>
2024-04-09 12:05:06 +0100

    feature 2 added

--- src/main/java/com/dkit/oop/sd2/DAOs/PlayerDaoInterface.java
+++ src/main/java/com/dkit/oop/sd2/DAOs/PlayerDaoInterface.java
@@ -6,9 +6,11 @@ import java.util.List;
 
 public interface PlayerDaoInterface
 {
-    List<Player> getAllPlayers() throws DaoException;
-    Player insertPlayer(Player p) throws DaoException;
-    Player updatePlayer(int id, Player p) throws DaoException;
+    List<Player> getAllPlayers() throws DaoException; // feature 1 (Roman)
+    Player findPlayerById(int id) throws DaoException; // feature 2 (Mila)
+    
+    Player insertPlayer(Player p) throws DaoException; // feature 4 (Roman)
+    Player updatePlayer(int id, Player p) throws DaoException; // feature 5 (Roman)
     boolean exists(int id) throws DaoException;
 }
 
