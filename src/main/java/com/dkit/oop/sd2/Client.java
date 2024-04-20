8a388f0ebc1b50c71d88f83a8aa634a0abf58179
LiverPork <D00264678@student.dkit.ie>
2024-04-20 18:45:14 +0300

    added feature 13

--- src/main/java/com/dkit/oop/sd2/Client.java
+++ src/main/java/com/dkit/oop/sd2/Client.java
@@ -1,10 +1,14 @@
 package com.dkit.oop.sd2;
 import com.dkit.oop.sd2.DTOs.Player;
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.List;
 import java.util.Scanner;
 
 /** Main author: Annita Mila Chuenglin */
@@ -20,6 +24,8 @@ public class Client {
                 OutputStream outputStream = socket.getOutputStream();
                 PrintWriter out = new PrintWriter(outputStream, true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream()); // Додано для addEntity
+                DataInputStream dis = new DataInputStream(socket.getInputStream()); // Додано для addEntity
                 Scanner scanner = new Scanner(System.in)) {
 
             System.out.println("The client is running and has connected to the server.");
@@ -31,7 +37,7 @@ public class Client {
                 System.out.println("3. Add an Entity");
                 System.out.println("4. Delete an Entity by Id");
                 System.out.println("5. Get Images List");
-                System.out.println("6. Exit Server");
+                System.out.println("6. Exit");
                 System.out.print("Enter option: ");
 
                 int option = scanner.nextInt();
@@ -68,11 +74,12 @@ public class Client {
                         String position = scanner.nextLine();
 
                         Player newPlayer = new Player(name, age, team, position);
-
                         String jsonRequest = JsonConverter.playerToJson(newPlayer);
+                        out.println("addEntity ");
+                        dos.writeUTF(jsonRequest);
+                        dos.flush();
 
-                        out.println("addEntity " + jsonRequest);
-                        String addResponse = in.readLine();
+                        String addResponse = dis.readUTF();
                         System.out.println("Server response: " + addResponse);
                         break;
 
@@ -83,14 +90,35 @@ public class Client {
                         out.println("deleteEntityById "+entityId);
                         response = in.readLine();
                         System.out.println("Player successfully deleted");
-                        return;
+                        break;
+                    /** Main author: Roman Manzhelii*/
                     case 5:
-                        System.out.println("Exiting....");
-                        return;
-                    /* Main author: Elga Jerusha Henry*/
+                        out.println("getImagesList");
+                        Gson gson = new Gson();
+                        String jsonResponse = in.readLine();
+
+                        //System.out.println("Raw JSON response from server: " + jsonResponse);
+
+                        Type type = new TypeToken<List<String>>() {}.getType();
+                        List<String> imagesList = gson.fromJson(jsonResponse, type);
+
+                        if (imagesList.isEmpty()) {
+                            System.out.println("No images found.");
+                        } else {
+                            System.out.println("Available images: ");
+                            imagesList.forEach(System.out::println);
+                        }
+
+                        System.out.print("Enter the name of the image to download: ");
+                        String imageName = scanner.nextLine();
+                        out.println("getFile " + imageName);
+                        receiveFile(imageName, socket);
+                        break;
+
+                    /** Main author: Elga Jerusha Henry*/
                     case 6:
-                        out.println("exit");
-                        System.out.println("Exiting the client and server. Goodbye!");
+                        System.out.println("Exiting the client. Goodbye!");
+                        socket.close();
                         System.exit(0);
                         break;
                     default:
@@ -102,6 +130,31 @@ public class Client {
             System.out.println(e);
         } catch (IOException e) {
             System.out.println(e);
+        }
+    }
+
+    /** Main author: Roman Manzhelii*/
+    private void receiveFile(String fileName, Socket socket) throws IOException {
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        String status = dis.readUTF();
+        if (!"OK".equals(status)) {
+            System.out.println(status);
+            return;
+        }
+        long fileLength = dis.readLong();
+        FileOutputStream fos = new FileOutputStream("downloaded/" + fileName);
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        long totalRead = 0;
+        while (totalRead < fileLength && (bytesRead = dis.read(buffer)) != -1) {
+            fos.write(buffer, 0, bytesRead);
+            totalRead += bytesRead;
+        }
+        fos.close();
+        if (totalRead == fileLength) {
+            System.out.println("Received file: " + fileName);
+        } else {
+            System.out.println("File transfer incomplete. Expected " + fileLength + " bytes, received " + totalRead + " bytes.");
         }
     }
 }
--- src/main/java/com/dkit/oop/sd2/Server.java
+++ src/main/java/com/dkit/oop/sd2/Server.java
@@ -1,15 +1,16 @@
 package com.dkit.oop.sd2;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.dkit.oop.sd2.DAOs.MySqlPlayerDao;
 import com.dkit.oop.sd2.DTOs.Player;
 import com.dkit.oop.sd2.Exceptions.DaoException;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 /** Main author: Annita Mila Chuenglin */
 public class Server {
@@ -32,65 +33,123 @@ public class Server {
     }
 
     public void start() throws DaoException {
-        try (ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-             Socket clientSocket = serverSocket.accept();
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-            System.out.println("The Server server has started and is waiting for a client to connect.");
-
-            String message = in.readLine();
-
-            System.out.println("The Server has received this message from a client: " + message);
-            System.out.println("The server is replying to the client.");
-
-            String[] request = message.split(" ");
-            /** Main author: Annita Mila Chuenglin */
-            if(request[0].equals("displayEntityById")) {
-                int entityId = Integer.parseInt(request[1]);
-                Player player = playerDao.findPlayerById(entityId);
-                String jsonPlayer = jsonConverter.playerToJson(player);
-                out.println(jsonPlayer);
-            }
-            /** Main author: Annita Mila Chuenglin */
-            else if(request[0].equals("displayAllEntities")) {
-                List<Player> allPlayersList = playerDao.getAllPlayers();
-                String allPlayers = jsonConverter.playersListToJson(allPlayersList);
-                out.println(allPlayers);
-            }
-            /** Main author: Annita Mila Chuenglin */
-            else if(request[0].equals("deleteEntityById")) {
-                int deleteId = Integer.parseInt(request[1]);
-                Player deletedPlayer = playerDao.deletePlayer(deleteId);
-                if(deletedPlayer != null)
-                {
-                    out.println("Player with ID" + deleteId +" deleted");
+        try (ServerSocket serverSocket = new ServerSocket(PORT_NUMBER)) {
+            System.out.println("The Server has started and is waiting for a client to connect.");
+            System.out.flush();
+            try (
+                    Socket clientSocket = serverSocket.accept();
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+                    DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+            ) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println("The Server has received this message from a client: " + inputLine);
+                    String[] request = inputLine.split(" ", 2);
+                switch (request[0]) {
+                    /** Main author: Annita Mila Chuenglin */
+                    case "displayEntityById":
+                        int entityId = Integer.parseInt(request[1]);
+                        Player player = playerDao.findPlayerById(entityId);
+                        String jsonPlayer = jsonConverter.playerToJson(player);
+                        out.println(jsonPlayer);
+                        break;
+                    /** Main author: Annita Mila Chuenglin */
+                    case "displayAllEntities":
+                        List<Player> allPlayersList = playerDao.getAllPlayers();
+                        String allPlayers = jsonConverter.playersListToJson(allPlayersList);
+                        out.println(allPlayers);
+                        break;
+                    /** Main author: Annita Mila Chuenglin */
+                    case "deleteEntityById":
+                        int deleteId = Integer.parseInt(request[1]);
+                        Player deletedPlayer = playerDao.deletePlayer(deleteId);
+                        if (deletedPlayer != null) {
+                            out.println("Player with ID " + deleteId + " deleted");
+                        } else {
+                            out.println("Player with ID " + deleteId + " not found.");
+                        }
+                        break;
+                    /** Main author: Elga Jerusha Henry */
+                    case "addEntity":
+                        String jsonRequest = dis.readUTF();
+                        try {
+                            System.out.println("Attempting to parse JSON: " + jsonRequest);
+                            Player newPlayer = JsonConverter.jsonToPlayer(jsonRequest);
+                            Player addedPlayer = playerDao.insertPlayer(newPlayer);
+                            if (addedPlayer != null) {
+                                String jsonResponse = JsonConverter.playerToJson(addedPlayer);
+                                dos.writeUTF(jsonResponse);
+                            } else {
+                                dos.writeUTF("Failed to add the entity");
+                            }
+                        } catch (JsonSyntaxException ex) {
+                            System.out.println("JSON parsing error: " + ex.getMessage());
+                            dos.writeUTF("Invalid JSON format: " + ex.getLocalizedMessage());
+                        } catch (Exception e) {
+                            System.out.println("Error parsing JSON: " + jsonRequest + "; Error: " + e.getMessage());
+                            dos.writeUTF("Invalid JSON format");
+                        }
+                        dos.flush();
+                        break;
+                    /** Main author: Roman Manzhelii*/
+                    case "getImagesList":
+                        File folder = new File("images");
+                        File[] listOfFiles = folder.listFiles();
+                        Gson gson = new Gson();
+                        if (listOfFiles != null) {
+                            List<String> fileList = Arrays.stream(listOfFiles)
+                                    .filter(File::isFile)
+                                    .map(File::getName)
+                                    .collect(Collectors.toList());
+                            String json = gson.toJson(fileList);
+                            out.println(json);
+                        } else {
+                            out.println(gson.toJson("No images found."));
+                        }
+                        break;
+                    /** Main author: Roman Manzhelii*/
+                    case "getFile":
+                        String fileName = request[1];
+                        sendFile(fileName, clientSocket);
+                        break;
+                    default:
+                        out.println("Unrecognised input");
+                        break;
                 }
-                else
-                {
-                    out.println("Player with ID " + deleteId + " not found.");
                 }
+                System.out.println("The Server is finished, and is exiting. Goodbye!");
             }
-            /* Main author: Elga Jerusha Henry */
-            else if (request[0].equals("addEntity")) {
-                String jsonRequest = request[1];
-                Player newPlayer = JsonConverter.jsonToPlayer(jsonRequest);
-                Player addedPlayer = playerDao.insertPlayer(newPlayer);
-                if (addedPlayer != null) {
-                    String jsonResponse = JsonConverter.playerToJson(addedPlayer);
-                    out.println(jsonResponse);
-                } else {
-                    out.println("Failed to add the entity");
-                }
-            }
-            else {
-                out.println("Unrecognised input");
-            }
-
-            System.out.println("The Server is finished, and is exiting. Goodbye!");
-
         } catch (IOException exception) {
-            System.out.println(exception);
+            System.out.println("Exception: " + exception.getMessage());
+            exception.printStackTrace();
         }
     }
+
+    /** Main author: Roman Manzhelii*/
+    private void sendFile(String fileName, Socket socket) throws IOException {
+        File file = new File("images/" + fileName);
+        if (!file.exists()) {
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            dos.writeUTF("File not found");
+            return;
+        }
+        long fileLength = file.length();
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        dos.writeUTF("OK");
+        dos.writeLong(fileLength);
+
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ((bytesRead = bis.read(buffer)) != -1) {
+            dos.write(buffer, 0, bytesRead);
+        }
+        bis.close();
+        dos.flush();
+        System.out.println("Sent file: " + fileName);
+    }
+
 }
